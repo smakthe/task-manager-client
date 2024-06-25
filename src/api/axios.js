b8e8ef369136e02c130ad6ce9cb691a7a0a2b802
smakthe <scmakra99@gmail.com>
2024-06-25 11:33:39 +0530

    Removed environment file

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: 'https://task-manager-api-ctij.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
